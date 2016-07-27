7fe12ebeefab88a96bbb047be6b2b9923d8b1b9e
adam t <Adam.Anh.Ta@gmail.com>
2016-07-27 16:42:56 -0700

    Update dashboard.py

--- portal/views/user/dashboard.py
+++ portal/views/user/dashboard.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-import os
-
 
 @login_required
 def dashboard(request):
