f58fac935e79db01761fcadab8305c88875bf620
adam <adam.anh.ta@gmail.com>
2016-08-26 12:11:39 -0700

    port settings fix

--- portal/views/user/portfolio_settings.py
+++ portal/views/user/portfolio_settings.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from portal.views.user.portfolio import get_top_portfolios
 from django.views.decorators.csrf import csrf_exempt
 from portal.views.user import get_top_portfolios
 
