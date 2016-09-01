d6de857fd628460e64676f59947f445012ce50ce
adam <adam.anh.ta@gmail.com>
2016-09-01 13:00:35 -0700

    fixing backtest blind

--- portal/views/user/backtest.py
+++ portal/views/user/backtest.py
@@ -56,7 +56,32 @@ def backtest(request):
                 alloc['value'] = percentStock
                 i+=1
                 # print(symbol)
+            stockLength = []
+            for stock in thisport:
+                stockLength.append("a")
+            extras = []
+            # testing purposes
+            # extras.append({'symbol':'symbol16','number':'16','ticker':'AAPL','percentStock':'5%'})
+            # extras.append({'symbol':'symbol17','number':'17','ticker':'AAPL','percentStock':'5%'})
+            # extras.append({'symbol':'symbol18','number':'18','ticker':'AAPL','percentStock':'5%'})
+            # extras.append({'symbol':'symbol19','number':'19','ticker':'AAPL','percentStock':'5%'})
+            # extras.append({'symbol':'symbol20','number':'20','ticker':'AAPL','percentStock':'5%'})
+            # extras.append({'symbol':'symbol21','number':'21','ticker':'AAPL','percentStock':'5%'})
+            if len(stockLength) > 15:
+                extra = len(stockLength) - 15
+                pin_tag = soup.topBorder
+                extras = []
+                for x in range(0,extra):
+                    sym = "symbol" + str(x)
+                    new_tag = {'symbol':sym, 'number':str(x), 'percentStock':percentStock, 'ticker':portStocks[15+int(x)]}
+                    extras.append(new_tag)
+                    new_tag = soup.new_tag("div", class="", value="")
+                    new_tag.insert_before(pin_tag)
                 # print(allocation)
     # print(soup.prettify().encode('ascii', 'ignore'))    # pageReturn = str(soup)
     # print(pageReturn)
-    return JsonResponse({'data': soup.prettify().encode('ascii', 'ignore')})
+    try:
+        print(extras)
+        return JsonResponse({'data': soup.prettify().encode('ascii', 'ignore'), 'extras':extras })
+    except UnboundLocalError:
+        return JsonResponse({'data': soup.prettify().encode('ascii', 'ignore') })
