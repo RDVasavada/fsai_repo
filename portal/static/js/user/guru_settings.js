9930c42712f0775742c7a7606bf81eafd32a6ca4
adam <adam@ailabs.co>
2016-11-06 13:41:37 -0800

    fixing guru_settings

--- portal/static/js/user/guru_settings.js
+++ portal/static/js/user/guru_settings.js
@@ -13,7 +13,7 @@ var get_guru = function(id) {
     type: 'POST',
     data: {'data':'adata'}
   }).done(function(portfolio) {
-      portfolio.data.forEach(function(stock,i=1) {
+      portfolio.data.forEach(function(stock,i) {
         var rtnstr = ("<tr><td>"+(i+1)+"</td><td>"+stock.symbol+"</td><td>"+stock.company+"</td><td>"+stock.value+"</td><td>"+stock.change+"</td><td>"+stock.exchange+"</td><td>"+stock['13f_date']+"</td><td>"+stock.impact+"</td><td>"+stock.share+"</td><td>"+stock.yield+"</td><td>"+stock.industry+"</td><td>"+stock.sector+"</td><td>"+stock.position+"</td><td>"+stock.pct+"</td><td>"+stock.mktcap+"</td></tr>")
         $("#pickPin").append(rtnstr)
       })
@@ -50,7 +50,7 @@ var openNav = function(id, chosenGuruName) {
     data: {'data':'adata'}
   }).done(function(data) {
     // console.log(data)
-      data.data.forEach(function(stock,i=1) {
+      data.data.forEach(function(stock,i) {
         var rtnstr = ("<tr><td>"+(i+1)+"</td><td>"+stock.symbol+"</td><td>"+stock.company+"</td><td>"+stock.value+"</td><td>"+stock.change+"</td><td>"+stock.exchange+"</td><td>"+stock['13f_date']+"</td><td>"+stock.impact+"</td><td>"+stock.share+"</td><td>"+stock.yield+"</td><td>"+stock.industry+"</td><td>"+stock.sector+"</td><td>"+stock.position+"</td><td>"+stock.pct+"</td><td>"+stock.mktcap+"</td></tr>")
         $("#pickPin").append(rtnstr)
       })
